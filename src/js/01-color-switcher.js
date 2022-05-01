396c9256bb20574c0e61dc0bc8a9959f46c377dc
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-05-01 09:38:28 +0300

    color-switcher ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+}
 
+refs.start.addEventListener('click', onStartClick);
+refs.stop.addEventListener('click', onStopClick);
+
+//встановлюємо змінну в загальному полі видимості, зоб можна було зупинити таймер за допомогою іншої функції
+let timerId = null;
+
+
+
+function onStartClick(){
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+
+function onStopClick() {
+    clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
